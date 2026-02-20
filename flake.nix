79258db1682bc24033f6c50cd834d62de300cdb6
Jack Kelly <jack@control-plane.io>
2026-02-20 09:06:51 +0000

    feat: update and add dev env

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ inputs.devshell.overlays.default ];
+        };
+        selfPkgs = self.packages.${system};
+
+        # packages
+        go = pkgs.go_1_25;
+      in
+      {
+        devShell = pkgs.devshell.mkShell {
+          devshell = {
+            name = "sandbox-probe";
+            motd = "";
+            packages = with pkgs; [
+              go
+              gcc
+              buf
+            ];
+          };
+        };
+      }
+    );
+}
