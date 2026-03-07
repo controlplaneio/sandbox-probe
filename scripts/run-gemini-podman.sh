0637bc0b282c457e5141aae32660bb0d3ef64e77
Jack Kelly <jack@control-plane.io>
2026-03-07 09:43:24 +0000

    fix: correct gemini scripts
    --prompt is missing run_shell_command tool access so need to use a
    different approach.

--- scripts/run-gemini-podman.sh
+++ scripts/run-gemini-podman.sh
@@ -2,10 +2,16 @@
 
 
 
-cp $1 $2
+cp $1 $2/sandbox-probe
 
 cd $2
 
 VERSION=$(gemini --version)
 
-GEMINI_SANDBOX=podman gemini -y --sandbox -p "Execute !$1 scan --tags version=${VERSION},tool=gemini"
+printf "!!!! please ensure your ./bin/sandbox-probe is either statically linked\nor only links to standard locations /lib /lib64\nso it can run inside the podman sandbox"
+
+# gemini doesn't allow tool call of `run_shell_command` when using non interactive prompts
+# there is `--prompt-interactive` which can take a value but even with `--yolo`
+# gemini will wait for user input and won't quit
+# instead we pipe output into interaction
+printf "Execute \`./sandbox-probe scan --tags version=%s,tool=gemini\` take no further action and exit /quit\n\n\n\n" "$VERSION" | GEMINI_SANDBOX=podman gemini -y --sandbox
--- scripts/run-gemini.sh
+++ scripts/run-gemini.sh
@@ -6,4 +6,8 @@ cd $2
 
 VERSION=$(gemini --version)
 
-gemini -y -p "Execute !$1 scan --tags version=${VERSION},tool=gemini"
+# gemini doesn't allow tool call of `run_shell_command` when using non interactive prompts
+# there is `--prompt-interactive` which can take a value but even with `--yolo`
+# gemini will wait for user input and won't quit
+# instead we pipe output into interaction
+printf "Execute \`./sandbox-probe scan --tags version=%s,tool=gemini\` take no further action and exit /quit\n\n\n\n" "$VERSION" | gemini --yolo
